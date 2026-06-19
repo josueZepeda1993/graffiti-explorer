26d90ab8a5e788c8a075332ec2d9ab17ff978cd2
Josue Zepeda <josuemaginzepeda1993@gmail.com>
2026-06-18 20:37:02 -0600

    Implementacion de busquedas dinamicas, por nombre, artista y ubicacion

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-//import '../models/mural.dart';
-import 'mural_detail_screen.dart';
+
+import '../models/mural.dart';
 import '../data/murals_data.dart';
-import 'map_screen.dart';
+import '../screens/map_screen.dart';
+import '../screens/mural_detail_screen.dart';
 import '../services/favorites_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,6 +14,31 @@ class HomeScreen extends StatefulWidget {
 
 }
 class _HomeScreenState extends State<HomeScreen>{
+  final TextEditingController _searchController = TextEditingController();
+  late List<Mural> muralesFiltrados;
+
+  @override
+  void initState(){
+    super.initState();
+
+    muralesFiltrados = List.from(murales);
+  }
+
+  void filtrarMurales(String texto){
+    final busqueda = texto.toLowerCase();
+    setState(() {
+      if (texto.isEmpty){
+        muralesFiltrados = List.from(murales);
+      }else{
+        muralesFiltrados = murales.where((mural){
+          return mural.nombre.toLowerCase().contains(busqueda) || 
+                 mural.artista.toLowerCase().contains(busqueda) || 
+                 mural.ubicacion.toLowerCase().contains(busqueda);
+        }).toList();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,13 +72,51 @@ class _HomeScreenState extends State<HomeScreen>{
             label: const Text('Ver Mapa'),
           ),
 
+          TextField(
+           controller: _searchController, 
+           onChanged: filtrarMurales,
+           decoration: InputDecoration(
+              hintText: 'Buscar Mural ...',
+              prefixIcon: const Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12)
+              ),
+           ),           
+          ),
+
           const SizedBox(height: 20),  
 
           Expanded(
-            child: ListView.builder(
-              itemCount: murales.length,
+            child: muralesFiltrados.isEmpty
+            ? const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.search_off,
+                      size: 64,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'No se encontraron murales',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Prueba con otro nombre, artista o ubicacion',
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+            )
+            : ListView.builder(
+              itemCount: muralesFiltrados.length,
               itemBuilder: (context, index) {
-                final mural = murales[index];
+                final mural = muralesFiltrados[index];
 
                 return Card(
                   child: ListTile(
